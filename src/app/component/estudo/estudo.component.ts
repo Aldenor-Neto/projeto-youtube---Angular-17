c02f55541d4de92e01d9da495ea7bc501f298458
aldenor-neto <aldenor05062010@gmail.com>
2024-07-12 15:26:40 -0300

    props biding

--- src/app/component/estudo/estudo.component.ts
+++ src/app/component/estudo/estudo.component.ts
@@ -10,8 +10,20 @@ import { Component } from '@angular/core';
 export class EstudoComponent {
 
   nome: String = "Escrito do canal";
+  imagem: String = "../../../assets/imagem home studio Aldenor Neto.jpg";
+  descricao: String = "imagem do stúdio Aldenor Neto";
 
-  mensagem(){
+  mensagem() {
     alert('estudando event biding');
+  }
+
+  alterarImagem(){
+if(this.imagem === "../../../assets/imagem home studio Aldenor Neto.jpg"){
+  this.imagem ="../../../assets/imagem kenosis.jpeg";
+  this.descricao = "imagem Kenôsis";
+} else{
+  this.imagem = "../../../assets/imagem home studio Aldenor Neto.jpg";
+  this.descricao = "imagem do stúdio Aldenor Neto";
+}
   }
 }
