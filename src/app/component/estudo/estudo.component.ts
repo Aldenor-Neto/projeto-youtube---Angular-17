9fec44854e09a0566dafe639b624984ad0da76fe
aldenor-neto <aldenor05062010@gmail.com>
2024-08-08 11:41:36 -0300

    Formulario formsModel

--- src/app/component/estudo/estudo.component.ts
+++ src/app/component/estudo/estudo.component.ts
@@ -1,18 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-estudo',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './estudo.component.html',
   styleUrl: './estudo.component.css'
 })
 export class EstudoComponent {
 
-cor: boolean = true;
-lista: String[] = ['Aldenor Neto', 'Fabrícia', 'Jônatas', 'Davi'];
-media: number = 3;
-linguagem: String = "php";
-
+  cor: boolean = true;
+  lista: String[] = ['Aldenor Neto', 'Fabrícia', 'Jônatas', 'Davi'];
+  media: number = 3;
+  linguagem: String = "php";
+  nome: String = "";
 }
