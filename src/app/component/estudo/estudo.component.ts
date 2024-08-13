874907204fe8c2c35c00959e4069f72bae3d18a2
aldenor-neto <aldenor05062010@gmail.com>
2024-08-13 14:35:30 -0300

    fixando conhecimento calculadora de imc

--- src/app/component/estudo/estudo.component.ts
+++ src/app/component/estudo/estudo.component.ts
@@ -11,9 +11,30 @@ import { FormsModule } from '@angular/forms';
 })
 export class EstudoComponent {
 
-  cor: boolean = true;
-  lista: String[] = ['Aldenor Neto', 'Fabrícia', 'Jônatas', 'Davi'];
-  media: number = 3;
-  linguagem: String = "php";
-  nome: String = "";
+  peso: number = 0;
+  altura: number = 0;
+  imc: number = 0;
+  mensagem: String = "";
+
+  calculaImc() {
+    this.imc = this.peso / (this.altura * this.altura);
+    this.imc = + this.imc.toFixed(1);
+
+    if(this.imc < 18.5){
+      this.mensagem = "desnutrição!";
+    } else if (this.imc <25){
+      this.mensagem = "peso ideal!";
+    } else if (this.imc <30){
+      this.mensagem = "sobre-peso!";
+    } else {
+      this.mensagem = "obesidade!";
+    }
+  }
+
+  novoCalculo(){
+this.peso = 0;
+this.altura = 0;
+this.imc = 0;
+this.mensagem = "";
+  }
 }
